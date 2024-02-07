e303f4fdfc94396332c5db669cbce92f28d4a319
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-07 15:24:32 +0530

    Data

---
+++ Data/SchoolDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RainbowSchoolWebApp.Model;
+
+namespace RainbowSchoolWebApp.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public SchoolDbContext (DbContextOptions<SchoolDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RainbowSchoolWebApp.Model.Student> Student { get; set; } = default!;
+    }
+}
