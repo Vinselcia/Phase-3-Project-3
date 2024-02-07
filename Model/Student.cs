5da280a285d96529062ef8927226a1ee23b4936c
Vinselcia <153624107+Vinselcia@users.noreply.github.com>
2024-02-07 15:25:31 +0530

    Models

---
+++ Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RainbowSchoolWebApp.Model
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        public int Student_Id {  get; set; }
+        public string ? Student_Name { get; set; }
+        [StringLength(50)]
+        public string ? Subject {  get; set; }
+        public double Marks {  get; set; }
+    }
+}
